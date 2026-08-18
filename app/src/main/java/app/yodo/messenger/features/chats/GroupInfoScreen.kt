8ba225370fb52425fe05e30ccb44452d978255dc
artemmetra2022 <artemmetra2022spb@gmail.com>
2026-08-18 21:09:46 +0300

    Fix

--- app/src/main/java/app/yodo/messenger/features/chats/GroupInfoScreen.kt
+++ app/src/main/java/app/yodo/messenger/features/chats/GroupInfoScreen.kt
@@ -152,7 +152,7 @@ fun GroupInfoScreen(
                             verticalAlignment = Alignment.CenterVertically
                         ) {
                             Icon(
-                                androidx.compose.material.icons.Icons.AutoMirrored.Filled.Forum,
+                                androidx.compose.material.icons.Icons.Filled.Forum,
                                 contentDescription = null,
                                 tint = MaterialTheme.colorScheme.primary
                             )
