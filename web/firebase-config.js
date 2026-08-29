348e07c2707680a0dcf56d4212f1cd57cbde3c19
artemmetra2022 <artemmetra2022spb@gmail.com>
2026-08-29 09:25:52 +0300

    web: firebaseConfig вынесен в отдельный firebase-config.js

---
+++ web/firebase-config.js
@@ -0,0 +1,12 @@
+// Конфигурация Firebase веб-клиента. Ключи веб-клиента Firebase публичны
+// по дизайну (доступ к данным ограничен firestore.rules), но вынесены в
+// отдельный файл, чтобы не смешивать с логикой приложения.
+window.FIREBASE_CONFIG = {
+  apiKey: "AIzaSyBN0R6R54f1Dah3vp7WrYrsY95e5NgMZA4",
+  authDomain: "yodomessenger.firebaseapp.com",
+  projectId: "yodomessenger",
+  storageBucket: "yodomessenger.firebasestorage.app",
+  messagingSenderId: "509907567167",
+  appId: "1:509907567167:web:b7ea079acbf1ab2272ae8a",
+  measurementId: "G-YGGHJ2BWEM",
+};
