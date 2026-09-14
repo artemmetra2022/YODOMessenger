0d56adbc1c2835199eaf8686ba099aee665ea574
artemmetra2022 <artemmetra2022spb@gmail.com>
2026-09-14 21:03:10 +0300

    Delete Desktop directory

--- Desktop/YODOMessenger-main/app/src/main/java/app/yodo/messenger/domain/model/TwoFactorState.kt
+++
@@ -1,6 +0,0 @@
-package app.yodo.messenger.domain.model
-
-/** Состояние двухфакторной аутентификации по email-коду. */
-data class TwoFactorState(
-    val enabled: Boolean = false
-)
--- Desktop/YODOMessenger-main/app/src/main/java/app/yodo/messenger/domain/model/UserPresence.kt
+++
@@ -1,6 +0,0 @@
-package app.yodo.messenger.domain.model
-
-data class UserPresence(
-    val isOnline: Boolean,
-    val lastSeenMillis: Long
-)
